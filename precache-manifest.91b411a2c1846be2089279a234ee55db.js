46a3e1f2f039f0a2a0c8241180be3c6a45933f0d
Jancarlos Acevedo <Jancarlos92@me.com>
2021-07-09 00:50:52 -0400

    Updates

---
+++ precache-manifest.91b411a2c1846be2089279a234ee55db.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed975881d5a316b0eadf13f9aa57f802",
+    "url": "/frontendPRIVATE/index.html"
+  },
+  {
+    "revision": "89964ae555baa77116c7",
+    "url": "/frontendPRIVATE/static/css/2.b836d782.chunk.css"
+  },
+  {
+    "revision": "c486b1e3194437c7a6a7",
+    "url": "/frontendPRIVATE/static/css/main.757bd3ed.chunk.css"
+  },
+  {
+    "revision": "89964ae555baa77116c7",
+    "url": "/frontendPRIVATE/static/js/2.a1b07deb.chunk.js"
+  },
+  {
+    "revision": "c486b1e3194437c7a6a7",
+    "url": "/frontendPRIVATE/static/js/main.16be4dd0.chunk.js"
+  },
+  {
+    "revision": "6a205ca29ea3e71521bc",
+    "url": "/frontendPRIVATE/static/js/runtime~main.7af5441c.js"
+  },
+  {
+    "revision": "2288ae6ce6fc1742d58f11fbdc406543",
+    "url": "/frontendPRIVATE/static/media/avatar.2288ae6c.png"
+  },
+  {
+    "revision": "94ef4d80f514a14a01f8593ee608b9ae",
+    "url": "/frontendPRIVATE/static/media/born.94ef4d80.svg"
+  },
+  {
+    "revision": "08ad67afbbb178e5ad13c66859d6b27c",
+    "url": "/frontendPRIVATE/static/media/comments.08ad67af.svg"
+  },
+  {
+    "revision": "0d77fc21680082061398bf88194b72dd",
+    "url": "/frontendPRIVATE/static/media/home-2@2x.0d77fc21.svg"
+  },
+  {
+    "revision": "890e7f353c7ad3ad471d786e139bffed",
+    "url": "/frontendPRIVATE/static/media/home.890e7f35.svg"
+  },
+  {
+    "revision": "62e940798159facb03b241c771c4ffd5",
+    "url": "/frontendPRIVATE/static/media/images.62e94079.svg"
+  },
+  {
+    "revision": "43430dbe658d69814f1c5a2ade0ab810",
+    "url": "/frontendPRIVATE/static/media/joined.43430dbe.svg"
+  },
+  {
+    "revision": "7c4adb49e0840b3e68dc1c232acd5ba3",
+    "url": "/frontendPRIVATE/static/media/leatherworkervid.7c4adb49.mp4"
+  },
+  {
+    "revision": "f0a32005c6665ff2cf84fd0fbc4f1568",
+    "url": "/frontendPRIVATE/static/media/like.f0a32005.svg"
+  },
+  {
+    "revision": "9e80692f3cdde007eed882186df4e230",
+    "url": "/frontendPRIVATE/static/media/logo.9e80692f.svg"
+  },
+  {
+    "revision": "4e22a60fe89496770bf82db9f5fc0288",
+    "url": "/frontendPRIVATE/static/media/message.4e22a60f.svg"
+  },
+  {
+    "revision": "d2dca68158103f0bc0c0886cd2697a38",
+    "url": "/frontendPRIVATE/static/media/mybuds-v2.d2dca681.png"
+  },
+  {
+    "revision": "f72c1ccbcfe15af5846d6be8565be1c2",
+    "url": "/frontendPRIVATE/static/media/mybudscell1.f72c1ccb.png"
+  },
+  {
+    "revision": "903d02e2d6462b0c4f86adcb6d0da4b9",
+    "url": "/frontendPRIVATE/static/media/mybudscell2.903d02e2.png"
+  },
+  {
+    "revision": "d2dca68158103f0bc0c0886cd2697a38",
+    "url": "/frontendPRIVATE/static/media/mybudslogo.d2dca681.png"
+  },
+  {
+    "revision": "24b9d96691732373e4c33e5162587783",
+    "url": "/frontendPRIVATE/static/media/notification.24b9d966.svg"
+  },
+  {
+    "revision": "53e68fd1e6e2cbc5600a2c72851cdd6b",
+    "url": "/frontendPRIVATE/static/media/notification@2x.53e68fd1.svg"
+  },
+  {
+    "revision": "663c50ef87424b39bc176f6a31d1ff5a",
+    "url": "/frontendPRIVATE/static/media/p1.663c50ef.jpg"
+  },
+  {
+    "revision": "8304b0ad8f29f67396c1b731b69c127f",
+    "url": "/frontendPRIVATE/static/media/p4.8304b0ad.jpg"
+  },
+  {
+    "revision": "2a7022cb0dcfd4f82b2d1e65f0e997e7",
+    "url": "/frontendPRIVATE/static/media/place.2a7022cb.svg"
+  },
+  {
+    "revision": "b55664a5b92468e339728337c8a4f04b",
+    "url": "/frontendPRIVATE/static/media/pressed@2x.b55664a5.svg"
+  },
+  {
+    "revision": "af8fe76c2dfd4bf41aadbadd0faac501",
+    "url": "/frontendPRIVATE/static/media/product-3@2x.af8fe76c.svg"
+  },
+  {
+    "revision": "77ad3b40ff445282ed2058c0785141ca",
+    "url": "/frontendPRIVATE/static/media/retweet.77ad3b40.svg"
+  },
+  {
+    "revision": "52a6274fe30b4d8571427cbc43431abe",
+    "url": "/frontendPRIVATE/static/media/search.52a6274f.svg"
+  },
+  {
+    "revision": "51bcd29e11c57bbe8c012b4462fbf8ba",
+    "url": "/frontendPRIVATE/static/media/search@2x.51bcd29e.svg"
+  },
+  {
+    "revision": "60966acd1aa743af460a454826f3021b",
+    "url": "/frontendPRIVATE/static/media/url.60966acd.svg"
+  },
+  {
+    "revision": "0d4a2d3144f9554c73dc95e9177ffce7",
+    "url": "/frontendPRIVATE/static/media/user@2x.0d4a2d31.svg"
+  }
+]);
