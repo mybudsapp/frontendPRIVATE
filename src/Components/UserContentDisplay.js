790f183602b005daf36f2cd69ea8a1bda71f8ae4
Jancarlos Acevedo <Jancarlos92@me.com>
2022-02-23 16:58:59 -0500

    charts

--- src/Components/UserContentDisplay.js
+++ src/Components/UserContentDisplay.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import {Responsive,Visibility, Label, Grid, Card, Segment, Menu, Image, Icon, Header, Divider, Button, Form, Input, TextArea, Select, Feed, Item} from "semantic-ui-react"
-
+import { Line, Bar, Radar} from "react-chartjs-2";
+import Chart from 'chart.js/auto'
 import {Link, withRouter} from 'react-router-dom'
 import StoreCard from "./StoreCard"
 import StrainCard from "./StrainCard"
@@ -259,16 +260,92 @@ render(){
         )
     } else if(this.props.activeItem === 'Feed'){
 
+        const linedata = {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+      datasets: [
+        {
+          label: "Last Year",
+          data: [33, 53, 85, 41, 44, 65],
+          fill: true,
+          backgroundColor: "rgba(75,192,192,0.2)",
+          borderColor: "rgba(75,192,192,1)"
+        },
+        {
+          label: "This Year",
+          data: [33, 25, 35, 51, 54, 76],
+          fill: false,
+          borderColor: "#742774"
+        }
+      ]
+    };
+
+    const bardata = {
+        labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+  datasets: [{
+    label: 'Sales',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
 
 
+
+const radardata = {
+  labels: [
+    'Apparel',
+    'Jewelery',
+    'FootWear',
+    'Designing',
+    'Electronics',
+    'Home Decor',
+    'Furniture'
+  ],
+  datasets: [{
+    label: 'Last Year',
+    data: [65, 59, 90, 81, 56, 55, 40],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+  }, {
+    label: 'This Year',
+    data: [28, 48, 40, 19, 96, 27, 100],
+    fill: true,
+    backgroundColor: 'rgba(54, 162, 235, 0.2)',
+    borderColor: 'rgb(54, 162, 235)',
+    pointBackgroundColor: 'rgb(54, 162, 235)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(54, 162, 235)'
+  }]
+};
         return(
 
 
-                  <Segment padded='very'>
+                  <Segment.Group>
                       <Segment>
                           <h1>This Will Be Your Feed...</h1>
-
-
                       </Segment>
                       <Feed>
                           {this.mobileScreen(getWidth)?
@@ -313,132 +390,35 @@ render(){
                  </Visibility>
                       }
 
-
-
-
-              <Feed.Event>
-                <Feed.Label>
-                  <img src='https://images.unsplash.com/photo-1535740451417-25dafb0667bb?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80' />
-                </Feed.Label>
-                <Feed.Content>
-                  <Feed.Summary>
-                    <Feed.User>Elliot Fu</Feed.User> added you as a friend
-                    <Feed.Date>1 Hour Ago</Feed.Date>
-                  </Feed.Summary>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />4 Likes
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-
-              <Feed.Event>
-                <Feed.Label image='https://images.unsplash.com/photo-1613437636004-bb0f5786558d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80' />
-                <Feed.Content>
-                  <Feed.Summary>
-                    <a href="/profile">Helen's Boutique</a> added <a>2 New Products</a>
-                    <Feed.Date>4 days ago</Feed.Date>
-                  </Feed.Summary>
-                  <Feed.Extra images>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1505740420928-5e560c06d30e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' />
-                    </a>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1523275335684-37898b6baf30?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=689&q=80' />
-                    </a>
-                  </Feed.Extra>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />1 Like
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-
-              <Feed.Event>
-                <Feed.Label image='https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80' />
-                <Feed.Content>
-                  <Feed.Summary>
-                    <a href="/profile">Drigo Marquez</a> added <a>2 new Photos</a>
-                    <Feed.Date>4 days ago</Feed.Date>
-                  </Feed.Summary>
-                  <Feed.Extra images>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1581954548122-4dff8989c0f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80' />
-                    </a>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1581954548218-415cd6ee5f4d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=80' />
-                    </a>
-                  </Feed.Extra>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />1 Like
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-
-              <Feed.Event>
-                <Feed.Label image='https://images.unsplash.com/photo-1437913135140-944c1ee62782?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80' />
-                <Feed.Content>
-                  <Feed.Summary
-                    date='2 Days Ago'
-                    user='Jenny Hess'
-                    content=' add you as a friend'
-                  />
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />8 Likes
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-
-              <Feed.Event>
-                <Feed.Label image='https://images.unsplash.com/photo-1609220136736-443140cffec6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' />
-                <Feed.Content>
-                  <Feed.Summary>
-                    <a href="/profile">Joe Henderson</a> reviewed <a href="/storeprofile">Dos Amigo's Watches</a>
-                    <Feed.Date>3 days ago</Feed.Date>
-                  </Feed.Summary>
-                  <Feed.Extra text>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </Feed.Extra>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />5 Likes
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-
-              <Feed.Event>
-                <Feed.Label image='https://images.unsplash.com/photo-1602256927451-407682b34c4b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80' />
-                <Feed.Content>
-                  <Feed.Summary>
-                    <a href="/profile">Justen Kitsune</a> added <a>2 new photos</a> of you
-                    <Feed.Date>4 days ago</Feed.Date>
-                  </Feed.Summary>
-                  <Feed.Extra images>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1517456793572-1d8efd6dc135?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80' />
-                    </a>
-                    <a href="/profile">
-                      <img src='https://images.unsplash.com/photo-1619537765491-c2b80c842e10?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=401&q=80' />
-                    </a>
-                  </Feed.Extra>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name='like' />
-                      41 Likes
-                    </Feed.Like>
-                  </Feed.Meta>
-                </Feed.Content>
-            </Feed.Event>
             </Feed>
 
-                  </Segment>
+
+
+
+            <Segment.Group>
+                <Segment>
+                    Interactions
+                </Segment>
+                <Segment>
+                        <Line data={linedata} />
+                </Segment>
+
+                <Segment>
+                    Sales
+                </Segment>
+                <Segment>
+                        <Bar data={bardata} />
+                </Segment>
+
+                <Segment>
+                    Most Bought Category
+                </Segment>
+                <Segment>
+                        <Radar data={radardata} />
+                </Segment>
+
+            </Segment.Group>
+                  </Segment.Group>
 
 
 
